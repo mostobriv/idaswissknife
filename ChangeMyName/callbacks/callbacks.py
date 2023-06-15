477cf2a7180cfc5e6eff3919d7271001eb490737
Max Stanchin <15954313+mostobriv@users.noreply.github.com>
2023-06-15 18:33:35 +0700

    Update callbacks.py

--- ChangeMyName/callbacks/callbacks.py
+++ ChangeMyName/callbacks/callbacks.py
@@ -13,9 +13,12 @@ class HexRaysCallbackManager(object):
         for h in self.__hexrays_hooks:
             h.unhook()
 
-    def register(self, hx_hook) -> None:
+    def register(self, hx_hook, quite=True) -> None:
         self.__hexrays_hooks.append(hx_hook)
-        hx_hook.hook()
+        res = hx_hook.hook()
+        if not quite:
+            # TODO: add name attribute to HexRaysEventHook to make pretty print.
+            print("[*] Registered %s hexrays-hook with status %d" % (hx_hook, res))
 
 
 hx_callback_manager = HexRaysCallbackManager()
